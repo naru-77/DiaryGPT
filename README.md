cb779de53fa72f0fef50d10e95928b95b0eab0bf
RenNishino <ren20021122@gmail.com>
2023-07-09 14:42:17 +0900

    音声入力,gptの質問,チャットみたいに表示

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # Diary
+
 日記アプリ
+
+# いきなり 403 になったら、キャッシュ削除してみる
+
+# 毎回起動時に行うこと?(れん)
+
+# 2 つ目の venv は仮想環境の名前
+
+python3 -m venv venv
+
+# この venv も仮想環境の名前
+
+source venv/bin/activate
+
+pip install flask
+
+# pip の version アップデート
+
+/Users/r.nishino/DiaryGPT/venv/bin/python3 -m pip install --upgrade pip
+
+pip install openai
+
+# 環境変数を設定
+
+export OPENAI_API_KEY=''
--- static/script.js
+++ static/script.js
@@ -1,3 +1,57 @@
-window.onload = function () {
-  alert("Hello, World!");
+const button = document.querySelector("#button");
+const conversation = document.querySelector("#conversation");
+
+function addUserText(text) {
+  const userDiv = document.createElement("div");
+  userDiv.setAttribute("id", "user");
+  userDiv.innerText = "User: " + text;
+  conversation.appendChild(userDiv);
+}
+
+function addGptText(text) {
+  const gptDiv = document.createElement("div");
+  gptDiv.setAttribute("id", "gpt");
+  gptDiv.innerText = "GPT-3: " + text;
+  conversation.appendChild(gptDiv);
+}
+
+button.onclick = () => {
+  const recognition = new window.webkitSpeechRecognition();
+  button.style.backgroundColor = "red"; //録音時のボタン色変える
+  recognition.onresult = (event) => {
+    const speech = event.results[0][0].transcript; //認識されたテキストを取得
+    fetch("/speech", {
+      method: "POST",
+      body: new URLSearchParams({ speech }),
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    })
+      .then((response) => response.text())
+      .then((text) => {
+        addUserText(text); //音声入力テキストをdiv要素で追加
+        const prompt = text;
+        button.style.backgroundColor = ""; //ボタン色リセット
+
+        fetch("/gpt", {
+          method: "POST",
+          body: new URLSearchParams({ prompt }),
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
+        })
+          .then((response) => response.text())
+          .then((gpt_response) => {
+            addGptText(gpt_response); //gptのレスポンスをdiv要素で追加
+          })
+          .catch((e) => {
+            console.error(e);
+          });
+      })
+      .catch((e) => {
+        console.error(e);
+      });
+  };
+
+  recognition.start();
 };
