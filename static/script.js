49c8900e93a4a247b4a9fcee111b5a240248e699
RenNishino <ren20021122@gmail.com>
2023-07-10 21:36:04 +0900

    読み上げを実装した

--- static/script.js
+++ static/script.js
@@ -26,7 +26,11 @@ button.onclick = () => {
   button.style.backgroundColor = "red"; //録音時のボタン色変える
   recognition.onresult = (event) => {
     const speech = event.results[0][0].transcript; //認識されたテキストを取得
-    fetch("/speech", {
+
+    addUserText(speech); //音声入力テキストをdiv要素で追加
+    button.style.backgroundColor = ""; //ボタン色リセット
+
+    fetch("/gpt", {
       method: "POST",
       body: new URLSearchParams({ speech }),
       headers: {
@@ -34,37 +38,20 @@ button.onclick = () => {
       },
     })
       .then((response) => response.text())
-      .then((text) => {
-        addUserText(text); //音声入力テキストをdiv要素で追加
-        const prompt = text;
-        button.style.backgroundColor = ""; //ボタン色リセット
+      .then((gpt_response) => {
+        addGptText(gpt_response); //gptのレスポンスをdiv要素で追加
 
-        fetch("/gpt", {
-          method: "POST",
-          body: new URLSearchParams({ prompt }),
-          headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-          },
-        })
-          .then((response) => response.text())
-          .then((gpt_response) => {
-            addGptText(gpt_response); //gptのレスポンスをdiv要素で追加
+        if ("speechSynthesis" in window) {
+          //読み上げに対応しているブラウザか確認
 
-            if ("speechSynthesis" in window) {
-              //読み上げに対応しているブラウザか確認
+          const msg = new SpeechSynthesisUtterance(); //音声出力
+          msg.text = gpt_response; // 読み上げるテキスト
+          msg.lang = "ja-JP"; // 日本語を指定
+          msg.rate = 0.9; // 速度 (0.1 - 10)
+          msg.pitch = 1.2; //ピッチ (0 - 2)声の高さ
 
-              const msg = new SpeechSynthesisUtterance(); //音声出力
-              msg.text = gpt_response; // 読み上げるテキスト
-              msg.lang = "ja-JP"; // 日本語を指定
-              msg.rate = 0.9; // 速度 (0.1 - 10)
-              msg.pitch = 1.2; //ピッチ (0 - 2)声の高さ
-
-              speechSynthesis.speak(msg);
-            }
-          })
-          .catch((e) => {
-            console.error(e);
-          });
+          speechSynthesis.speak(msg);
+        }
       })
       .catch((e) => {
         console.error(e);
